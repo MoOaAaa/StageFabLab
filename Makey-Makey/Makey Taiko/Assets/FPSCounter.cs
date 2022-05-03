664fd4b874a56d1ef68622dc053fa72040d62aff
MoOaAaa <71814380+MoOaAaa@users.noreply.github.com>
2022-05-03 12:58:55 -0400

    affichage fps

---
+++ Makey-Makey/Makey Taiko/Assets/FPSCounter.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour
+{
+    public Text fpsText;
+    public float deltaTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        fpsText.text = Mathf.Ceil(fps).ToString();
+    }
+}
