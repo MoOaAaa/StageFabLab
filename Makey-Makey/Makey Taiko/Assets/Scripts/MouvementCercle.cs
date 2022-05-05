22aede9dbb3c1567c206288b22423878ddd98483
MoOaAaa <71814380+MoOaAaa@users.noreply.github.com>
2022-05-05 09:26:07 -0400

    on stop tout

--- Makey-Makey/Makey Taiko/Assets/Scripts/MouvementCercle.cs
+++ Makey-Makey/Makey Taiko/Assets/Scripts/MouvementCercle.cs
@@ -6,6 +6,14 @@ public class MouvementCercle : MonoBehaviour
     public int speed = 7;
     public string couleur;
 
+    public float songBpm;
+    public float secPerBeat;
+    public float songPosition;
+    public float songPositionInBeat;
+    public float dspSongTime;
+    public float firstBeatOffset;
+    public float previousDspTime;
+    public AudioSource musicSource;
     // Setup video
     public GameObject videoPlayer;
 
@@ -13,15 +21,32 @@ public class MouvementCercle : MonoBehaviour
     {
         Application.targetFrameRate = 60;
         videoPlayer = GameObject.FindGameObjectWithTag("Video");
+
+        musicSource.GetComponent<AudioSource>();
+        secPerBeat = 60f / songBpm;
+        dspSongTime = (float)AudioSettings.dspTime;
+        previousDspTime = (float)AudioSettings.dspTime;
+
+        musicSource.Play();
     }
 
     void Update()
     {
+        float avancementMusique = (float)AudioSettings.dspTime - previousDspTime;
         // Si la video est prête alors
         if (videoPlayer.GetComponent<VideoPlayer>().waitForFirstFrame)
         {
-            transform.Translate(Vector3.left * 0.016f * speed);
+            transform.Translate(Vector3.left * avancementMusique * speed);
         }
+
+        songPosition = (float)(AudioSettings.dspTime - dspSongTime - firstBeatOffset);
+
+        songPositionInBeat = songPosition / secPerBeat;
+        Debug.Log("song position in beats : " + songPositionInBeat);
+        Debug.Log(dspSongTime);
+        Debug.Log(AudioSettings.dspTime);
+
+        previousDspTime = (float)AudioSettings.dspTime;
     }
 }
 
