95b7a59f873c94644ad97b6dd44ab04e10a888a0
Jolan Thomassin <98430140+JolanThomassin@users.noreply.github.com>
2022-05-09 11:11:37 -0400

    Ajout du code de la machine

---
+++ Machine cocktails/sketch_may09a/sketch_may09a.ino
@@ -0,0 +1,59 @@
+#include <LiquidCrystal.h>
+
+int digPin8 = 8;
+int digPin9 = 9;
+int digPin10 = 10;
+int contrast = 60;
+
+LiquidCrystal lcd(11, 12, 2, 3, 4, 5);
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(digPin8, HIGH);
+  pinMode(digPin9, HIGH);
+  pinMode(digPin10 , HIGH);
+
+  lcd.begin(16, 2);
+  analogWrite(6, contrast);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  digitalWrite(digPin8, HIGH);
+  lcd.setCursor(0, 0);
+  lcd.print("Moteur pin 8 en cours");
+  delay(1000);
+  lcd.clear();
+  
+  digitalWrite(digPin8, LOW);
+  lcd.setCursor(0, 0);
+  lcd.print("Moteur pin 8 terminé");
+  delay(1000);
+  lcd.clear();
+  
+  digitalWrite(digPin9, HIGH);
+  lcd.setCursor(0, 0);
+  lcd.print("Moteur pin 9 en cours");
+  delay(1000);
+  lcd.clear();
+  
+  
+  digitalWrite(digPin9, LOW);
+  lcd.setCursor(0, 0);
+  lcd.print("Moteur pin 9 terminé");
+  delay(1000);
+  lcd.clear();
+  
+  digitalWrite(digPin10, HIGH);
+  lcd.setCursor(0, 0);
+  lcd.print("Moteur pin 10 en cours");
+  delay(1000);
+  lcd.clear();
+  
+  digitalWrite(digPin10, LOW);
+  lcd.setCursor(0, 0);
+  lcd.print("Moteur pin 10 terminé");
+  delay(1000);
+  lcd.clear();
+}
